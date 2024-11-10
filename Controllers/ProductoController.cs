8283b4264b8d86459bf915905fa34208e0c2f031
Mauro Orlando <orlandomauro2000@gmail.com>
2024-11-09 21:28:12 -0300

    commit 2

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using tl2_tp6_2024_MauroOrlando2000.Models;
 namespace tl2_tp6_2024_MauroOrlando2000.Controllers;
 
+[Route("controller")]
 public class ProductoController : Controller
 {
     private IProductoRepository repositorioProductos;
@@ -12,32 +13,33 @@ public class ProductoController : Controller
         repositorioProductos = new ProductoRepository();
     }
 
-    [HttpPost]
-    public IActionResult CrearProducto(Producto producto)
-    {
-        return View(repositorioProductos.CrearProducto(producto));
-    }
-
     [HttpGet]
-    public IActionResult ObtenerProductos()
+    public IActionResult Index()
     {
         return View(repositorioProductos.ObtenerProductos());
     }
 
-    [HttpGet]
-    public IActionResult Buscar(int id)
+    [HttpGet("/CrearProducto")]
+    public IActionResult CrearProducto()
     {
-        return View(repositorioProductos.Buscar(id));
+        return View();
     }
 
-    [HttpPut]
-    public IActionResult ModificarProducto([FromRoute]int id, [FromBody]Producto producto)
+    [HttpPost("/CrearProducto")]
+    public IActionResult CrearProducto([FromForm]Producto producto)
+    {
+        repositorioProductos.CrearProducto(producto);
+        return View();
+    }
+
+    [HttpPut("/ModificarProducto")]
+    public IActionResult ModificarProducto([FromRoute]int id, [FromForm]Producto producto)
     {
         return View(repositorioProductos.ModificarProducto(id, producto));
     }
 
     [HttpDelete]
-    public IActionResult EliminarProducto(int id)
+    public IActionResult EliminarProducto([FromRoute]int id)
     {
         return View(repositorioProductos.EliminarProducto(id));
     }
